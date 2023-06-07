384dd3f3f3544326b84914010447e9ead4bbe80a
Ben Chung <ckfinite@gmail.com>
2023-06-06 20:39:26 -0400

    Fix (and test!) the chained preconditioner implementation

--- src/preconditioning/chained.jl
+++ src/preconditioning/chained.jl
@@ -10,41 +10,52 @@ struct ChainedState{T,P<:Problem{T}, STs <: Tuple{Vararg{<:PreconditionerState{T
 	states::STs
 end
 
-function precondition(p::Problem, pc::Chained{PCs}) where PCs
-	intermediate_problems = Problem[]
+function precondition(p::Problem{T}, pcs::Chained{PCs}) where {T, PCs}
+	intermediate_problems = Problem{T}[]
 	states = PreconditionerState[]
 	current_problem = p
-	for (i, pc) in enumerate(pc.preconditioners)
+	for (i, pc) in enumerate(pcs.preconditioners)
 		(current_problem, state) = precondition(current_problem, pc)
-		if i < length(pc.preconditioners)
+		if i < length(pcs.preconditioners)
 			push!(intermediate_problems, current_problem)
 		end
 		push!(states, state)
 	end
 	return (current_problem, ChainedState(intermediate_problems, (states...,)))
 end
-function precondition!(src::Problem, tgt::Problem, pc::ChainedState)
+function precondition!(src::Problem, tgt::Problem, pcs::ChainedState)
+	if length(pcs.states) == 0
+		return # we literally didn't do anything
+	end
+	if length(pcs.problems) == 0
+		precondition!(src, tgt, pcs.states[1])
+		return
+	end
 	current_state = src
-	current_target = first(pc.problems)
-	for (i, pc) in enumerate(pc.states)
-		if i == length(pc.problems)
+	current_target = first(pcs.problems)
+	for (i, state) in enumerate(pcs.states)
+		if i == length(pcs.states)
 			current_target = tgt
 		else 
-			current_target = pc.problems[i+1]
+			current_target = pcs.problems[i]
 		end
-		precondition!(current_state, current_target, pc)
+		precondition!(current_state, current_target, state)
 		current_state = current_target
 	end
 end
 
 function extract(next_pc::Function, pc::ChainedState{T}, p::Union{Primal, Dual}, ind::Int)::T where T 
 	# next_pc :: Func{Int, T}
-	@inline function index_mapping(::Val{IDX}) where IDX
-		if IDX == length(pc.states)
+	@inline function index_mapping(v::Val{IDX}) where IDX
+		if IDX == length(pc.states)+1
 			return next_pc
 		else
-			return (index) -> extract(index_mapping(Val(pc_index+1)), pc.states[IDX], p, index)
+			return (index) -> extract(index_mapping(Val(IDX+1)), pc.states[IDX], p, index)
 		end
 	end
-	return extract(index_mapping(Val(1)), first(pc.states), p, ind)
+	if length(pc.states) == 0
+		return next_pc(ind)
+	end
+	res = extract(index_mapping(Val(2)), first(pc.states), p, ind)
+	return res
 end
